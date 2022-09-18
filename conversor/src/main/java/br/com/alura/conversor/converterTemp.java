6050904bd86d9f4670279e13a339477cdcf3e641
Ticiano Diniz <ticciano@gmail.com>
2022-09-18 15:19:30 -0300

    First version

---
+++ conversor/src/main/java/br/com/alura/conversor/converterTemp.java
@@ -0,0 +1,40 @@
+package br.com.alura.conversor;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class converterTemp
+ */
+@WebServlet(name = "ConverterTemp", urlPatterns = { "/converterTemp" })
+public class converterTemp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String stringTemp = request.getParameter("tempCelsius");
+		DecimalFormat numberFormat = new DecimalFormat("#.00");
+		double celsius,fahrenheit;
+		
+		celsius = Integer.parseInt(stringTemp);
+		fahrenheit = (celsius*1.8)+32;
+		numberFormat.format(fahrenheit);
+		
+		
+		//chamar JSP
+    RequestDispatcher rd = request.getRequestDispatcher("/result.jsp");
+    request.setAttribute("result", String.valueOf(fahrenheit));
+    request.setAttribute("celsius", String.valueOf(celsius));
+    rd.forward(request, response);
+	}
+
+}
---
+++ conversor/src/main/webapp/result.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+		System.out.println(request.getAttribute("result"));
+		String result = (String) request.getAttribute("result");
+		String celsius = (String) request.getAttribute("celsius");
+	
+	%>
+	
+		<h1>Resultado</h1>
+		<h2><%= celsius %>C representam  <%= result %>F</h2>
+		<a href="/conversor/index.html">início</a>
+	
+</body>
+</html>
